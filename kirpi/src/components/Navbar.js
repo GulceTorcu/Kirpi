3e9d8fdb2194683eed357248ba87e54d425eda04
GulceTorcu <gulcetorcu@hotmail.com>
2023-04-23 21:39:38 +0200

    Some navbar design changes

--- kirpi/src/components/Navbar.js
+++ kirpi/src/components/Navbar.js
@@ -5,34 +5,39 @@ import { CDBSidebar, CDBSidebarContent, CDBSidebarFooter, CDBSidebarHeader, CDBS
 
 const Navbar = () => {
     return (
-        <div className="sidebar">
-            <CDBSidebar textColor="#8e8d8f" backgroundColor="#2f273d">
-                <CDBSidebarHeader prefix={<i className="fa fa-bars fa-large"></i>}>
-                    <a href="/" className="header">
-                        Kirpi
-                    </a>
-                </CDBSidebarHeader>
-                <CDBSidebarContent>
-                    <CDBSidebarMenu>
-                        <NavLink to="/" activeClassName="activeClicked">
-                            <CDBSidebarMenuItem>
-                                Dashboard
-                            </CDBSidebarMenuItem>
-                        </NavLink>
-                        <NavLink to="/about" activeClassName="activeClicked">
-                            <CDBSidebarMenuItem>
-                                About
-                            </CDBSidebarMenuItem>
-                        </NavLink>
-                    </CDBSidebarMenu>
-                </CDBSidebarContent>
-                <CDBSidebarFooter className="footer">
-                    <div>
-                        Kirpi 2023
-                    </div>
-                </CDBSidebarFooter>
-            </CDBSidebar>
-        </div >
+        <>
+        <div className="header">
+        </div>
+            <div className="sidebar">
+                <CDBSidebar textColor="#8e8d8f" backgroundColor="#2f273d">
+                    <CDBSidebarHeader prefix={<i className="fa fa-bars fa-large"></i>}>
+                        <a href="/" className="navbarHeader">
+                            Kirpi
+                            
+                        </a>
+                    </CDBSidebarHeader>
+                    <CDBSidebarContent>
+                        <CDBSidebarMenu>
+                            <NavLink to="/" activeClassName="activeClicked">
+                                <CDBSidebarMenuItem>
+                                    Dashboard
+                                </CDBSidebarMenuItem>
+                            </NavLink>
+                            <NavLink to="/about" activeClassName="activeClicked">
+                                <CDBSidebarMenuItem>
+                                    About
+                                </CDBSidebarMenuItem>
+                            </NavLink>
+                        </CDBSidebarMenu>
+                    </CDBSidebarContent>
+                    <CDBSidebarFooter className="footer">
+                        <div>
+                            Kirpi 2023
+                        </div>
+                    </CDBSidebarFooter>
+                </CDBSidebar>
+            </div >
+        </>
     )
 }
 
